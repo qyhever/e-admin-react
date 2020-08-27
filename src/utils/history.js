c3b597b1276ede22c663afd9f887f0bea16035b0
kikc <15927700473@163.com>
2020-08-27 11:12:49 +0800

    Update history.js

--- src/utils/history.js
+++ src/utils/history.js
@@ -3,7 +3,7 @@ import { createBrowserHistory, createHashHistory } from 'history'
 console.log(process.env)
 let history = null
 if (process.env.IS_HASH) {
-  history = createHashHistory
+  history = createHashHistory()
 } else {
   history = createBrowserHistory({
     basename: process.env.PUBLIC_URL
